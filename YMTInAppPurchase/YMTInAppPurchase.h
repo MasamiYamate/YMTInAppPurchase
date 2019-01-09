2d8733e8628903b3e56ec2f704466e6c35084822
山手政実 <intel_i486_dx2@ezweb.ne.jp>
2019-01-09 14:44:07 +0900

    initialize commit

---
+++ YMTInAppPurchase/YMTInAppPurchase.h
@@ -0,0 +1,19 @@
+//
+//  YMTInAppPurchase.h
+//  YMTInAppPurchase
+//
+//  Created by MasamiYamate on 2019/01/04.
+//  Copyright © 2019 MasamiYamate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YMTInAppPurchase.
+FOUNDATION_EXPORT double YMTInAppPurchaseVersionNumber;
+
+//! Project version string for YMTInAppPurchase.
+FOUNDATION_EXPORT const unsigned char YMTInAppPurchaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YMTInAppPurchase/PublicHeader.h>
+
+
